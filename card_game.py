245ad42d4b21d6d53db74304f9b1f44578f7f2f2
Mani-Amanda <135225894+Mani-Amanda@users.noreply.github.com>
2024-10-31 19:37:42 +0530

    Card functions and Class Deck

--- card_game.py
+++ card_game.py
@@ -17,3 +17,44 @@ class Card:
         """suit + value are ints"""
         self.value = v
         self.suit = s
+    
+    def __lt__(self, c2):
+        if self.value < c2.value:
+            return True
+        if self.value == c2.value:
+            if self.suit < c2.suit:
+                return True
+            else:
+                return False
+        return False
+
+    def __gt__(self, c2):
+        if self.value > c2.value:
+            return True
+        if self.value == c2.value:
+            if self.suit > c2.suit:
+                return True
+            else:
+                return False
+        return False
+
+    def __repr__(self):
+        v = self.values[self.value] +\
+            " of " + \
+            self.suits[self.suit]
+        return v
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        for i in range(2, 15):
+            for j in range(4):
+                self.cards\
+                    .append(Card(i,
+                                 j))
+        shuffle(self.cards)
+
+    def rm_card(self):
+        if len(self.cards) == 0:
+            return
+        return self.cards.pop()
