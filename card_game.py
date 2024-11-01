9c66b3cf6bd0665922822f78a1ac6846d3012ad9
Mani-Amanda <135225894+Mani-Amanda@users.noreply.github.com>
2024-11-01 21:51:13 +0530

    Class Player and Class Game

--- card_game.py
+++ card_game.py
@@ -58,3 +58,31 @@ class Deck:
         if len(self.cards) == 0:
             return
         return self.cards.pop()
+    
+class Player:
+    def __init__(self, name):
+        self.wins = 0
+        self.card = None
+        self.name = name
+
+
+class Game:
+    def __init__(self):
+        name1 = input("p1 name ")
+        name2 = input("p2 name ")
+        self.deck = Deck()
+        self.p1 = Player(name1)
+        self.p2 = Player(name2)
+
+    def wins(self, winner):
+        w = "{} wins this round"
+        w = w.format(winner)
+        print(w)
+
+    def draw(self, p1n, p1c, p2n, p2c):
+        d = "{} drew {} {} drew {}"
+        d = d.format(p1n,
+                     p1c,
+                     p2n,
+                     p2c)
+        print(d)
